a6452770986e85d392be8407d6a39c641c6e9df7
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-03-13 15:33:01 +0700

    examples/Ruby: add publish with ruby gem

---
+++ examples/Ruby/ci/publish-rubygems.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p "$HOME/.gem"
+printf -- "---\n:rubygems_api_key: ${GEM_HOST_API_KEY}\n" > "$HOME/.gem/credentials"
+chmod 0600 "$HOME/.gem/credentials"
+gem build *.gemspec
+gem push *.gem
